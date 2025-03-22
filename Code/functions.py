0a9c003bd940f4f194132ef628b77361bdc078ac
Fabian Harrab <fabian@Fabians-MacBook-Air.local>
2025-03-22 11:43:01 +0100

    Base code for the calculator
    This is the base code i wrote for my calculator project. I plan on building more functionality on this base in the future such as:
    
    -UI
    -visual representation of functions
    -whatever comes to my mind that would be useful

---
+++ Code/functions.py
@@ -0,0 +1,73 @@
+import math
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+def square(a):
+    return a ** 2
+
+def sqrt(a):
+    return math.sqrt(a)
+
+def power(a, b):
+    return a ** b
+
+def factorial(a):
+    return math.factorial(a)
+
+def sin(a):
+    return math.sin(a)
+
+def cos(a):
+    return math.cos(a)
+
+def tan(a):
+    return math.tan(a)
+
+def asin(a):
+    return math.asin(a)
+
+def acos(a):
+    return math.acos(a)
+
+def atan(a):
+    return math.atan(a)
+
+def mod(a, b):
+    return a % b
+
+def neg(a):
+    return -a
+
+def exp(a):
+    return math.exp(a)
+
+def reciprocal(a):
+    return 1 / a
+
+def log(a):
+    return math.log(a)
+
+def degrees(a):
+    return math.degrees(a)
+
+def radians(a):
+    return math.radians(a)
+
+def pi():
+    return math.pi
+
+def e():
+    return math.e
+
+def parentheses(a):
+    return a
